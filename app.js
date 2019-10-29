88ad1dd44be0cefd50544df40cf39d908e050fe3
giselromina <giselromina@gmail.com>
2019-10-29 16:44:24 +0100

    "Solucionado eslint"

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my Nodemon API!');
+  res.send('Welcome to my Nodemon API!');
 });
 
 app.listen(port, () => {
-    console.log(`Running on port ${port}`);
+  console.log(`Running on port ${port}`);
 });
