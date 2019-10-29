98b57d68b0a479a8eb03c230616f61f4d91894d1
giselromina <giselromina@gmail.com>
2019-10-29 03:28:22 +0100

    commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my Nodemon API!');
+    res.send('Welcome to my  API!');
 });
 
 app.listen(port, () => {
